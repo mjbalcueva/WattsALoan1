eadb4d1c214db07360c95e2efeaa06c4eb72f0e5
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-18 00:48:00 +0800

    feat: #82 - 100

---
+++ WattsALoan1/WattsALoan.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WattsALoan1
+{
+    partial class WattsALoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPayments = new System.Windows.Forms.Button();
+            this.btnAllocations = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPayments
+            // 
+            this.btnPayments.Location = new System.Drawing.Point(120, 62);
+            this.btnPayments.Name = "btnPayments";
+            this.btnPayments.Size = new System.Drawing.Size(270, 95);
+            this.btnPayments.TabIndex = 0;
+            this.btnPayments.Text = "Loan Payments";
+            this.btnPayments.UseVisualStyleBackColor = true;
+            this.btnPayments.Click += new System.EventHandler(this.btnPayments_Click);
+            // 
+            // btnAllocations
+            // 
+            this.btnAllocations.Location = new System.Drawing.Point(410, 62);
+            this.btnAllocations.Name = "btnAllocations";
+            this.btnAllocations.Size = new System.Drawing.Size(270, 95);
+            this.btnAllocations.TabIndex = 0;
+            this.btnAllocations.Text = "Loan Allocations";
+            this.btnAllocations.UseVisualStyleBackColor = true;
+            this.btnAllocations.Click += new System.EventHandler(this.btnAllocations_Click);
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Location = new System.Drawing.Point(120, 177);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(270, 95);
+            this.btnCustomers.TabIndex = 0;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Location = new System.Drawing.Point(410, 177);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(270, 95);
+            this.btnEmployees.TabIndex = 0;
+            this.btnEmployees.Text = "Employees";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(265, 293);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(270, 95);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // WattsALoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAllocations);
+            this.Controls.Add(this.btnCustomers);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnEmployees);
+            this.Controls.Add(this.btnPayments);
+            this.Name = "WattsALoan";
+            this.Text = "Watts A Loan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPayments;
+        private System.Windows.Forms.Button btnAllocations;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ WattsALoan1/WattsALoan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WattsALoan1
+{
+    public partial class WattsALoan : Form
+    {
+        public WattsALoan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPayments_Click(object sender, EventArgs e)
+        {
+            Payments frmPayments = new Payments();
+            frmPayments.ShowDialog();
+        }
+
+        private void btnAllocations_Click(object sender, EventArgs e)
+        {
+            LoansAllocations frmLoans = new LoansAllocations();
+            frmLoans.ShowDialog();
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            Customers frmClients = new Customers();
+            frmClients.ShowDialog();
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            Employees frmStaff = new Employees();
+            frmStaff.ShowDialog();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
