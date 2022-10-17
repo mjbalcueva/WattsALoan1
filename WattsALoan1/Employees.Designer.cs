9845e58c3956a01a418df4a79bfedfab721b3177
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-17 22:38:02 +0800

    feat: #6 - 15

---
+++ WattsALoan1/Employees.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WattsALoan1
+{
+    partial class Employees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dsEmployees = new System.Data.DataSet();
+            this.tblEmployee = new System.Data.DataTable();
+            this.colEmployeeNumber = new System.Data.DataColumn();
+            this.colFirstName = new System.Data.DataColumn();
+            this.colLastName = new System.Data.DataColumn();
+            this.colTitle = new System.Data.DataColumn();
+            this.colHourlySalary = new System.Data.DataColumn();
+            this.colEmployeeDetails = new System.Data.DataColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dsEmployees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dsEmployees
+            // 
+            this.dsEmployees.DataSetName = "Employees";
+            this.dsEmployees.Tables.AddRange(new System.Data.DataTable[] {
+            this.tblEmployee});
+            // 
+            // tblEmployee
+            // 
+            this.tblEmployee.Columns.AddRange(new System.Data.DataColumn[] {
+            this.colEmployeeNumber,
+            this.colFirstName,
+            this.colLastName,
+            this.colTitle,
+            this.colHourlySalary,
+            this.colEmployeeDetails});
+            this.tblEmployee.Constraints.AddRange(new System.Data.Constraint[] {
+            new System.Data.UniqueConstraint("Constraint1", new string[] {
+                        "EmployeeNumber"}, false)});
+            this.tblEmployee.TableName = "Employee";
+            // 
+            // colEmployeeNumber
+            // 
+            this.colEmployeeNumber.AllowDBNull = false;
+            this.colEmployeeNumber.ColumnName = "EmployeeNumber";
+            // 
+            // colFirstName
+            // 
+            this.colFirstName.ColumnName = "FirstName";
+            // 
+            // colLastName
+            // 
+            this.colLastName.AllowDBNull = false;
+            this.colLastName.ColumnName = "LastName";
+            // 
+            // colTitle
+            // 
+            this.colTitle.ColumnName = "Title";
+            // 
+            // colHourlySalary
+            // 
+            this.colHourlySalary.ColumnName = "HourlySalary";
+            this.colHourlySalary.DataType = typeof(double);
+            this.colHourlySalary.DefaultValue = "8.75";
+            // 
+            // colEmployeeDetails
+            // 
+            this.colEmployeeDetails.ColumnName = "EmployeeDetails";
+            this.colEmployeeDetails.Expression = "EmployeeNumber + \': \' +FirstName + \' \' + LastName";
+            this.colEmployeeDetails.ReadOnly = true;
+            // 
+            // Employees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Employees";
+            this.Text = "Employees";
+            ((System.ComponentModel.ISupportInitialize)(this.dsEmployees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblEmployee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dsEmployees;
+        private System.Data.DataTable tblEmployee;
+        private System.Data.DataColumn colEmployeeNumber;
+        private System.Data.DataColumn colFirstName;
+        private System.Data.DataColumn colLastName;
+        private System.Data.DataColumn colTitle;
+        private System.Data.DataColumn colHourlySalary;
+        private System.Data.DataColumn colEmployeeDetails;
+    }
+}
