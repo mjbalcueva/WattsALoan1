720d042644f43e4dfbc3477b4153294142578620
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-17 22:22:22 +0800

    feat: #1 - 10

---
+++ WattsALoan1/LoansAllocations.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WattsALoan1
+{
+    partial class LoansAllocations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dsLoansAllocations = new System.Data.DataSet();
+            this.tblLoan = new System.Data.DataTable();
+            this.colDateAllocated = new System.Data.DataColumn();
+            this.colLoanNumber = new System.Data.DataColumn();
+            this.colPreparedBy = new System.Data.DataColumn();
+            this.colPreparedFor = new System.Data.DataColumn();
+            this.colPrincipal = new System.Data.DataColumn();
+            this.colInterestRate = new System.Data.DataColumn();
+            this.colPeriods = new System.Data.DataColumn();
+            this.colInterestEarned = new System.Data.DataColumn();
+            this.colFutureValue = new System.Data.DataColumn();
+            this.colMonthlyPayment = new System.Data.DataColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dsLoansAllocations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblLoan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dsLoansAllocations
+            // 
+            this.dsLoansAllocations.DataSetName = "LoansAllocations";
+            this.dsLoansAllocations.Tables.AddRange(new System.Data.DataTable[] {
+            this.tblLoan});
+            // 
+            // tblLoan
+            // 
+            this.tblLoan.Columns.AddRange(new System.Data.DataColumn[] {
+            this.colDateAllocated,
+            this.colLoanNumber,
+            this.colPreparedBy,
+            this.colPreparedFor,
+            this.colPrincipal,
+            this.colInterestRate,
+            this.colPeriods,
+            this.colInterestEarned,
+            this.colFutureValue,
+            this.colMonthlyPayment});
+            this.tblLoan.TableName = "Loan";
+            // 
+            // colDateAllocated
+            // 
+            this.colDateAllocated.ColumnName = "DateAllocated";
+            // 
+            // colLoanNumber
+            // 
+            this.colLoanNumber.ColumnName = "LoanNumber";
+            // 
+            // colPreparedBy
+            // 
+            this.colPreparedBy.ColumnName = "PreparedBy";
+            // 
+            // colPreparedFor
+            // 
+            this.colPreparedFor.ColumnName = "PreparedFor";
+            // 
+            // colPrincipal
+            // 
+            this.colPrincipal.ColumnName = "Principal";
+            // 
+            // colInterestRate
+            // 
+            this.colInterestRate.ColumnName = "InterestRate";
+            // 
+            // colPeriods
+            // 
+            this.colPeriods.ColumnName = "Periods";
+            // 
+            // colInterestEarned
+            // 
+            this.colInterestEarned.ColumnName = "InterestEarned";
+            // 
+            // colFutureValue
+            // 
+            this.colFutureValue.ColumnName = "FutureValue";
+            // 
+            // colMonthlyPayment
+            // 
+            this.colMonthlyPayment.ColumnName = "MonthlyPayment";
+            // 
+            // LoansAllocations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "LoansAllocations";
+            this.Text = "LoansAllocations";
+            ((System.ComponentModel.ISupportInitialize)(this.dsLoansAllocations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblLoan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dsLoansAllocations;
+        private System.Data.DataTable tblLoan;
+        private System.Data.DataColumn colDateAllocated;
+        private System.Data.DataColumn colLoanNumber;
+        private System.Data.DataColumn colPreparedBy;
+        private System.Data.DataColumn colPreparedFor;
+        private System.Data.DataColumn colPrincipal;
+        private System.Data.DataColumn colInterestRate;
+        private System.Data.DataColumn colPeriods;
+        private System.Data.DataColumn colInterestEarned;
+        private System.Data.DataColumn colFutureValue;
+        private System.Data.DataColumn colMonthlyPayment;
+    }
+}
